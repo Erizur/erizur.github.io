0f5d4ef4dfc37a971957a2baac7d449d9cf2c332
AM_Erizur <sajoudks@outlook.com>
2026-02-07 01:47:49 -0500

    Site updates

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var darkmode = false;
 
 function clickBrightChange() {
-    var element = document.body;
-    darkmode = !darkmode;
+  var element = document.body;
+  darkmode = !darkmode;
 
-    if (darkmode == true) element.setAttribute('dark', "");
-    else element.removeAttribute('dark');
-    
-    localStorage.setItem('darkmode', darkmode);
+  if (darkmode == true) element.setAttribute("dark", "");
+  else element.removeAttribute("dark");
+
+  localStorage.setItem("darkmode", darkmode);
 }
 
-window.onload = function() {
-    if (localStorage.getItem('darkmode') === 'true') darkmode = true;
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("darkmode") === "true") darkmode = true;
 
-    if (darkmode == true) document.body.setAttribute('dark', "");
-    else document.body.removeAttribute('dark');
-};
+  if (darkmode == true) document.body.setAttribute("dark", "");
+  else document.body.removeAttribute("dark");
+});
